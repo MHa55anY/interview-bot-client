15d4eed066e4992a72d9763d33275974f22e67bc
Hassan Yusuf <mhyyusuf3@gmail.com>
2024-05-09 03:46:55 +0530

    Enable insertion of audio received from server to the bot track

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,68 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo } from "react";
 import "./App.css";
 import Conference from "./Conference";
 import Footer from "./Footer";
 import JoinForm from "./JoinForm";
-import { selectIsConnectedToRoom, useHMSActions, useHMSStore } from "@100mslive/react-sdk";
-import AudioPlayer from "./AudioPlayer";
+import { selectIsConnectedToRoom, useHMSActions, useHMSStore, selectPeers } from "@100mslive/react-sdk";
 
 export default function App() {
   const isConnected = useHMSStore(selectIsConnectedToRoom);
   const hmsActions = useHMSActions();
-  const [audioSrc, setAudioSrc] = useState('');
+  const peers = useHMSStore(selectPeers)
+  const host = peers.find((p) => p.roleName === 'host');
+  const audioContext = useMemo(() => new AudioContext(), []);
+  const ws = useMemo(() => {
+    const websocket = new WebSocket('ws://localhost:8081');
+    hmsActions.setAppData("wsConn", websocket);
+    return websocket;
+  }, [hmsActions])
+
+  async function createAudioBuffer(arrayBuffer: ArrayBuffer, audioContext: AudioContext): Promise<AudioBuffer> {
+    return await audioContext.decodeAudioData(arrayBuffer);
+  }
+
+  // Function to convert the AudioBuffer to a MediaStream
+  function bufferToStream(audioBuffer: AudioBuffer, audioContext: AudioContext): MediaStream {
+    // Create an AudioBufferSourceNode and set its buffer
+    const source = audioContext.createBufferSource();
+    source.buffer = audioBuffer;
+
+    // Create a MediaStreamDestination node
+    const destination = audioContext.createMediaStreamDestination();
+
+    // Connect the AudioBufferSourceNode to the destination
+    source.connect(destination);
+
+    // Start the audio source
+    source.start(3);
+
+    // Get the media stream from the destination
+    return destination.stream;
+  }
+
+  console.log("Added track", peers);
+  
 
   useEffect(() => {
-    const ws = new WebSocket('ws://localhost:8081');
-    hmsActions.setAppData("wsConn", ws);
+    console.log("triggered useffect")
     ws.onopen = () => console.log("Connection established!!");
-    ws.onmessage = async (event) => {
-      const audioBlob = new Blob([event.data], { type: 'audio/mpeg' });
-      const blobUrl = URL.createObjectURL(audioBlob);
-      setAudioSrc(blobUrl);
-    };
+    if(host?.isLocal)
+      ws.onmessage = (event: MessageEvent<BlobPart>) => {
+        const audioBlob = new Blob([event.data]);
+        audioBlob.arrayBuffer()
+          .then((buffer) => createAudioBuffer(buffer, audioContext))
+          .then((audioBuffer) => {
+            const mediaStream: MediaStream = bufferToStream(audioBuffer, audioContext);
+            const audioTrack: MediaStreamTrack = mediaStream.getAudioTracks()[0]; // Extracting the audio track
+            return audioTrack;
+          })
+          .then((track) => hmsActions.addTrack(track, 'regular'))
+          .then(() => console.log("TRACK ADDEDD!"))
+      };
     ws.onclose = () => {
       console.log('Connection closed');
     };
-  }, [hmsActions]);
+  }, [audioContext, hmsActions, host?.isLocal, isConnected, ws]);
 
   return (
     <div className="App">
@@ -35,8 +74,6 @@ export default function App() {
       ) : (
         <JoinForm />
       )}
-      <br />
-      <AudioPlayer audioSrc={audioSrc} />
     </div>
   );
 }
